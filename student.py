2bc05f2ef13b40c6a31d1e1d9862ffc6624efa02
Yagavi Kannadhasan <yagavi1994@gmail.com>
2024-08-02 22:48:56 +0000

    Error correction

--- student.py
+++ student.py
@@ -20,8 +20,17 @@ class Student:
     def email(self):
         return f"{self._first_name.lower()}.{self._last_name.lower()}@email.com"
 
-    def apply_extension(self, day):
-        self.end_date += timedelta(days=day)
-
     def alert_santa(self):
         self.naughty_list = True
+
+    def apply_extension(self, days):
+        self.end_date += timedelta(days=days)
+
+    def course_schedule(self):
+        response = requests.get(
+            f"https://company.com/course-schedule/{self._last_name}/{self._first_name}")
+
+        if response.ok:
+            return response.text
+        else:
+            return "Something went wrong"
